bd70380ce6ab68ac45e0dc2aa85612a3e575282b
Saisruthi <mothesaisruthi678@gmail.com>
2021-12-05 15:54:44 +0530

    message

---
+++ 2021-12-05/dataflowformbrowswer/externalfun4.js
@@ -0,0 +1,62 @@
+function  add()
+{
+	var  n1 = document.getElementById("num1").value
+	var n2 = document.getElementById("num2").value
+	var  res  = parseInt(n1) + parseInt(n2)
+
+	document.write("number1 =" + n1 +" <br></br>")
+	document.write("number2 =" + n2 + "<br></br>")
+	document.write(  "addition =" +  res )
+}
+
+function  sub()
+{
+	var  n1 = document.getElementById("num1").value
+	var n2 = document.getElementById("num2").value
+	var  res  = (n1) -(n2)
+
+	document.write("number1 ="+ n1 +" <br></br>")
+	document.write("number2 =" + n2 + "<br></br>")
+	document.write("subtraction=" + res)
+
+
+}
+function  multi()
+{
+	var  n1 = document.getElementById("num1").value
+	var n2 = document.getElementById("num2").value
+	var  res  =(n1) *(n2)
+	document.write("number1 =" + n1 +" <br></br>")
+	document.write("number2 =" + n2 + "<br></br>")
+	document.write("mutiplication=" + res)
+	
+}
+function  divi()
+{
+	var  n1 = document.getElementById("num1").value
+	var n2 = document.getElementById("num2").value
+	var  res  = (n1) / (n2)
+
+	document.write("number1 =" + n1 +" <br></br>")
+	document.write("number2 =" + n2 + "<br></br>")
+	document.write("division=" + res)
+	
+}
+function  mod()
+{
+	var  n1 = document.getElementById("num1").value
+	var n2 = document.getElementById("num2").value
+	var  res  = (n1) %(n2)
+
+	document.write("number1 =" + n1 +" <br></br>")
+	document.write("number2 =" + n2 + "<br></br>")
+	document.write("modulo =" + res)
+}
+
+	
+
+
+	
+
+	
+		
